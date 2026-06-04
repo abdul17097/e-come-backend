4c6540ced8de3d39d7fdd2782f02cede37b10e6b
abdul17097 <103925027+abdul17097@users.noreply.github.com>
2026-06-04 08:58:53 +0500

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN || "*",
+    origin: "https://e-come-frontend.vercel.app/" || "*",
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: [
